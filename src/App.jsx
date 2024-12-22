9ac948ddd8f091849fcf6bc78d3b919613dac84b
Simarjot Singh <simu7060@gmail.com>
2024-12-22 17:51:42 +0530

    Login working

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/login";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
+}
+
+export default App;
