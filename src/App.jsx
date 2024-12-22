78c2929cf33f798652b6ea2bd3ac19e2207e64b2
Simarjot Singh <simu7060@gmail.com>
2024-12-22 17:54:55 +0530

    Fixed import in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Login from "./components/login";
+import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 
 function App() {
