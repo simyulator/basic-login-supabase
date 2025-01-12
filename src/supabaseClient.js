9575993ed4cbea418145b91d19999a61dd2fc15f
Simarjot Singh <simu7060@gmail.com>
2025-01-12 16:13:12 +0530

    env variabled fixed

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Replace these with your actual Supabase project details
-// const supabaseUrl = "https://uassczygdyjpxpkzzpnx.supabase.co";
 const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL;
-console.log(supabaseUrl);
 const supabaseAnonKey = import.meta.env.VITE_APP_API_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
